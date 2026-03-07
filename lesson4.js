763fa506c7fe41a053596e7724472f85b6489cf4
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-03-07 13:48:14 +0900

    Booleanについて

---
+++ lesson4.js
@@ -0,0 +1,15 @@
+// Boolean型について
+// booleanはシンプルで、trueかfalseの2種類しかない
+let isActiveGame = false;
+console.log(isActiveGame);
+
+isActiveGame = true;
+console.log(isActiveGame);
+
+
+
+// 変数のデータの型は変えられる！
+// 使う場面は稀だが、変えられるってことを覚えておこう！
+let numPuppies = 23; // Number
+numPuppies = false; // Booleanに変身！
+numPuppies = 100; // 再びNumberに変身!
