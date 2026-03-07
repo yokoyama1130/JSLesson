62f12b169353a6d237a5f1bc71ccabde5c207033
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-03-07 12:38:02 +0900

    JSをVSCode内で実行するためにNode.jsをインストール

---
+++ lesson1.js
@@ -0,0 +1,14 @@
+// nvmをダウンロードしてインストールする：
+// curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+
+// シェルを再起動する代わりに実行する
+// \. "$HOME/.nvm/nvm.sh"
+
+// Node.jsをダウンロードしてインストールする：
+//nvm install 24
+
+// Node.jsのバージョンを確認する：
+// node -v # "v24.14.0"が表示される。
+
+// npmのバージョンを確認する：
+// npm -v # "11.9.0"が表示される。
