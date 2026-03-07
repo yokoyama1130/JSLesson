db8b7fa945f094e65ea1fba6a4c9f29c00174432
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-03-07 12:49:16 +0900

    基本的な演算

--- lesson1.js
+++ lesson1.js
@@ -13,6 +13,9 @@
 // npmのバージョンを確認する：
 // npm -v # "11.9.0"が表示される。
 
+// // ターミナルで、node lesson1.js を実行すると出力される
+
+
 
 // GitHubのリモートリポジトリに保存するためのコマンド
 // git init
@@ -21,3 +24,32 @@
 // git remote add origin git@github.com:ユーザー名/リポジトリ名.git
 // git branch -M main
 // git push -u origin main
+
+
+
+// 基本演算
+// 足し算
+addition = 1 + 1
+console.log(addition)
+
+// 引き算
+subtraction = 10 - 5
+console.log(subtraction)
+
+// 掛け算
+multiplication = 2 * 3
+console.log(multiplication)
+
+// 割り算
+division = 8 / 2
+console.log(division)
+
+// 余り
+remainder = 5 % 2
+console.log(remainder)
+
+// べき演算
+exponentiation = 2 ** 10
+console.log(exponentiation)
+
+// ターミナルで、node lesson1.js を実行すると出力される
