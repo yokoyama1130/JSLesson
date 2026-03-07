48f167acd70658223db310a4846aededa0c32ee7
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-03-07 15:13:01 +0900

    引数が必要なメソッド

--- lesson6.js
+++ lesson6.js
@@ -70,3 +70,45 @@ let greeting = "    hello world!!!    "
 console.log(greeting.trim().toUpperCase());
 
 
+
+
+// Stringメソッドに引数を渡す
+// メソッドの括弧の中に引数を渡すこともできる。引数を渡すことで動作するメソッドもある。
+// 例）indexOfは引数で渡した文字列が最初に現れるインデックスを返してくれる
+let tvShow = "catdog";
+// この場合はcatが初めて出てくるのが添字が0のところなので0が返ってくる
+console.log(tvShow.indexOf("cat"));
+// dogは3で出てくるから3が返ってくる
+console.log(tvShow.indexOf("dog"));
+// その文字がない場合は-1が返ってくる
+console.log(tvShow.indexOf("z"));
+
+// 最初の文字だけしか出てこない（↓の場合、途中のhのインデックスが出てこない）
+console.log("hello happy world".indexOf("h"));
+
+
+
+// sliceメソッドは文字列の取り出すことができる（括弧に渡すものが二つ）
+// 最初の引数は取り出しを開始する位置を示す0から始まるインデックス
+// 2個目の引数は取り出しを終える前の0から始まるインデックス（2個目は省略可能。省略した時は最後まで切り取る）
+console.log("hello happy world!!!".slice(6));
+// 最後の指定するときは指定したインデックスの文字は含まない。
+console.log("hello happy world!!!".slice(6, 11));
+// -を使えば後ろから数えて取り出すこともできる
+console.log("hello happy world!!!".slice(-8));
+
+
+
+// replaceメソッドは文字列を置き換えられる
+// 1つ目の引数に置き換えたい文字列、2個目の引数に置き換える文字列を入力する（置き換えるのは最初に見つかった文字列だけ）
+let hello2 = "hello";
+console.log("hello");
+let hello3 = hello2.replace("hello", "good night");
+console.log(hello3);
+
+
+
+// repeatメソッド（指定した回数分文字列を繰り返す）
+console.log(hello2.repeat(10));
+
+
