c6e89ddb4e986b6fc59953bafba469d984932f71
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-03-15 16:34:07 +0900

    ダブルイコールとトリプルイコール

--- lesson10.js
+++ lesson10.js
@@ -13,3 +13,15 @@
 // trueかfalseが返ってくる（結果は全てBoolean）
 console.log(1 > 3);
 console.log(1 < 3);
+
+// ==（ダブルイコール）は型までは区別しない
+console.log(1 == "1");
+console.log(0 == "");
+console.log(0 == false);
+console.log(null == undefined);
+
+// ===（トリプルイコール）は型まで区別する
+console.log(1 === "1");
+console.log(0 === "");
+
+// データの型を意識するのが大事なので、一旦トリプルイコール＝＝＝を使う。
