ceb914a478a55d5e8b81b5f5a958b5b7724dae60
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-03-15 15:47:00 +0900

    undefinedとnull

--- lesson8.js
+++ lesson8.js
@@ -1,1 +1,16 @@
 // undefinedとnull
+// nullとは
+// 意図的に値が存在しないことを表します（使う時は代入する）
+
+// undefinedとは
+// まだ値が代入されていない変数はundefined型となる
+console.log(null);
+console.log(undefined);
+
+// 変数に何も入れずに宣言したままだとundefinedになる。
+// つまりundefinedはJavaScript的にわからないよって言ってる感じ
+let x;
+console.log(x);
+
+// nullは意図的に値がないことを表す。
+
