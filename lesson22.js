d2e27225f7d22b2aefd6e8cb99b193f7b2ec25f0
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-03-18 19:54:58 +0900

    配列の入門

---
+++ lesson22.js
@@ -0,0 +1,17 @@
+// Array（配列）~初めてのデータ構造~
+// 配列とは
+// 順序を持った「値のコレクション」
+// 例）SNSでのコメント一覧、ゲームの中のステージ一覧、プレイリストの中の歌一覧
+
+// 空の配列を作成
+let student = [];
+
+// stringの配列
+let colors = ["red", "orange", "yellow"];
+
+// Numberの配列
+let lottoNums = [19, 22, 56, 12, 51];
+
+// ごちゃ混ぜの配列
+let stuff = [true, 68, "cat", null];
+
