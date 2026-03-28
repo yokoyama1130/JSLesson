3d7547312110a52364e419b8efc476172844888d
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-03-29 00:58:04 +0900

    多次元配列

---
+++ lesson28.js
@@ -0,0 +1,9 @@
+// 配列のネスト（多次元配列）
+// 配列の中に配列を入れることもできる
+
+// まるばつゲーム
+const gameBoard = [["X", "O", "X"], ["o", null, "X"], ["O", "O", "X"]];
+console.log(gameBoard);
+
+// 取り出すとき
+console.log(gameBoard[1][1]);
