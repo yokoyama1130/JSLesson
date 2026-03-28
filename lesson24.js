98ccbb9aaf5ae535ac6bc74eb8a06a743a1b853e
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-03-28 23:04:18 +0900

    配列のメソッド（先頭編）

--- lesson24.js
+++ lesson24.js
@@ -24,3 +24,31 @@ console.log(movieLine);
 // 末尾を取り除く（引数なし）
 movieLine.pop();
 console.log(movieLine);
+
+// 取り出したものを変数に入れることもできる
+let person = movieLine.pop();
+console.log(person);
+console.log(movieLine);
+
+
+
+// 配列のメソッド（先頭編）
+// 先頭を取り除く
+movieLine.shift();
+console.log(movieLine);
+
+// もちろん取り出したのを変数に入れることもできる
+let nextPerson = movieLine.shift();
+console.log(nextPerson);
+console.log(movieLine);
+
+
+// 末尾に要素を追加
+movieLine.push("hayashi", "mori", "oda", "kato");
+console.log(movieLine);
+
+// 先頭に要素を追加
+movieLine.unshift("vip");
+console.log(movieLine);
+
+
