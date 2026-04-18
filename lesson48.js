78831484cc8f1d1e89fa69a15280a9b95e32cc59
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-04-18 19:30:59 +0900

    DOM入門

---
+++ lesson48.js
@@ -0,0 +1,30 @@
+// DOM入門
+
+// DOM入門
+// DOMはWEBページをJavaScriptで表現したもの
+// webページとJavaScriptを接続するもの
+// 言ってしまえばJavaScriptで扱えるオブジェクトの集まり
+
+
+
+// documentオブジェクト
+// documentオブジェクトがDOMの世界へのエントリーポートになっている
+// webページのあらゆるコンテンツを表していて、便利なプロパティやメソッドも多数用意されている。
+
+
+
+// 要素の取得（getElementById）
+// IDを元にオブジェクトを取ってきてくれる
+
+
+
+// getElementsTagNameとgetElementsClassName
+// getElementsTagNameはタグで取ってくる
+
+
+
+// querySelector
+// 単一の要素を取得できるセレクターメソッド（最初に見つけた一つを取得する）
+
+// querySelectorAll
+// querySelector同様だが、対象となる要素全てを取得するセレクターメソッド
