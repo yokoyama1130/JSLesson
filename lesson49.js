3aff7e72edfaf31b9b5fa971da8e0c104f4c6b22
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-04-20 18:21:11 +0900

    フォームのイベント

--- lesson49.js
+++ lesson49.js
@@ -16,6 +16,59 @@ button.addEventListener("click", function() {
 
 
 
+// イベントのthis
 
 
+
+// キーボードオブジェクトとイベントオブジェクト
+
+
+
+// フォームイベント
+const tweetForm = document.querySelector("#tweetForm");
+// 投稿を入れる場所のタグも取ってくる
+const tweets = document.querySelector("#tweets");
+tweetForm.addEventListener("submit", function(e) {
+    // デフォルトの動作を止める
+    e.preventDefault();
+
+    // // これをいちいち全部やってくのはめんどくさい
+    // // 他にもっと簡単な方法がある
+    // // 入力したものを取ってくる
+    // const usernameInput = document.querySelectorAll("input")[0];
+    // const tweetInput = document.querySelectorAll("input")[1];
+    // // 入力されたのはinput要素のvalueに保存される。
+    // console.log(usernameInput.value);
+    // console.log(tweetInput.value);
+
+    // もっと簡単な方法
+    console.log(tweetForm.elements.username.value);
+
+
+    // 一覧を作成する
+    // まず取ってくる
+    const username = tweetForm.elements.username.value;
+    const tweet = tweetForm.elements.tweet.value;
+
+    // タグの作成
+    const li = document.createElement("li");
+    const bTag = document.createElement("b");
+    
+    // bにユーザー名を載せる
+    bTag.append(username);
+    // liに投稿を載せる
+    li.append(bTag);
+    // bTagの中に投稿を入れる
+    li.append(` - ${tweet}`);
+
+    // 最後にHTMLにあるulタグに入れる
+    tweets.append(li);
+
+    // これは確認
+    console.log(bTag);
+
+    // 最後に入力されたものをリセットさせたい
+    tweetForm.elements.username.value = "";
+    tweetForm.elements.tweet.value = "";
+});
 
